0d00223ce125672a4b442166349c5f43aa8c2204
chaasfr <chaasfr@gmail.com>
2025-01-27 17:32:43 +0100

    aaded basic stdio reader

--- repl.go
+++ repl.go
@@ -1,9 +1,31 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"bufio"
+	"os"
+	"fmt"
+)
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	trimLowerText := strings.Fields(lowerText)
 	return trimLowerText
+}
+
+func replStart(){
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		textClean := cleanInput(text)
+
+		if len(textClean) > 0 {
+			toPrint := fmt.Sprintf("Your command was: %s", textClean[0])
+			fmt.Println(toPrint)
+		} else {
+			fmt.Println("Please provide a command")
+		}
+	}
 }
